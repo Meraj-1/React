50e482b0a20e32adcc9ecfb59e64f25c6246acb5
Mohd Meraj <mohammadmerajansari352@gmail.com>
2024-12-05 22:20:08 +0530

    fetching api

--- public/common/contast.js
+++ public/common/contast.js
@@ -1,7 +1,3 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const LINKEDIN_URL = "https://www.linkedin.com/in/bharat2044/";
-
 // Swiggy API for to get Restaurant data
 export const SWIGGY_API_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,50 +1,72 @@
 import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { CiSearch } from "react-icons/ci";
-import RestaurantList from "../utils/mockdata";
 import Shimmer from "./Shimmer";
+import { SWIGGY_API_URL, SWIGGY_REST_API_PATH } from "../utils/constant";
 
 const Body = () => {
-    const [searchText, setSearchText] = useState("");
-    const [filteredRestaurants, setFilteredRestaurants] = useState(RestaurantList);
-
-    const handleSearchChange = (e) => {
-        setSearchText(e.target.value);
-    };
-
-    useEffect(() => {
-        const updatedList = RestaurantList.filter((restaurant) => 
-            restaurant.info.name.toLowerCase().includes(searchText.toLowerCase()) ||
-            restaurant.info.areaName.toLowerCase().includes(searchText.toLowerCase()) ||
-            restaurant.info.cuisines.join(", ").toLowerCase().includes(searchText.toLowerCase())
+    const [restaurantList, setRestaurantList] = useState([]);
+    const [searchRestaurant, setSearchRestaurant] = useState("");
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+    const [restaurantName, setRestaurantName] = useState("");
+  
+    const fetchData = async () => {
+        try {
+          const data = await fetch(SWIGGY_API_URL);
+          const json = await data.json();
+          const restaurants = ("json?." + SWIGGY_REST_API_PATH) || [];
+    
+          setRestaurantList(restaurants);
+          setFilteredRestaurants(restaurants);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+    
+      useEffect(() => {
+        fetchData();
+      }, []);
+    
+      const handleSearch = () => {
+        const filtered = restaurantList.filter((res) =>
+          res.info.name.toLowerCase().includes(searchRestaurant.toLowerCase())
         );
-        setFilteredRestaurants(updatedList);
-    }, [searchText]);
-
-
-    return RestaurantList.length === 0 ? (
-        <Shimmer/>
-    ) : (
+    
+        setFilteredRestaurants(filtered);
+        setSearchRestaurant(""); // Clear the search input box after search
+        setRestaurantName(searchRestaurant);
+      };
+    
+      // Conditional rendering using ternary operator
+      return restaurantList.length === 0 ? (
+        <Shimmer />
+      ) : (
         <div className="body">
-            <div className="search-box">
-                <input
-                    placeholder="Search a restaurant you want"
-                    value={searchText}
-                    type="text"
-                    onChange={handleSearchChange}
+          <div className="search-box">
+            <input
+              type="text"
+              value={searchRestaurant}
+              onChange={(e) => setSearchRestaurant(e.target.value)}
+              placeholder="search a restaurant you want..."
+            />
+            <button className="search" onClick={handleSearch}>
+              Search
+            </button>
+          </div>
+    
+          <div className="restaurant-container">
+            {filteredRestaurants.length !== 0 ? (
+              filteredRestaurants.map((restaurant) => (
+                <RestaurantCard
+                  key={restaurant?.info?.id}
+                  {...restaurant?.info}
                 />
-                <CiSearch className="search-icon" />
-            </div>
-            <div className="restaurant-container">
-                {filteredRestaurants.map((restaurant) => (
-                    <RestaurantCard
-                        key={restaurant.info.id}
-                        restaurantData={restaurant}
-                    />
-                ))}
-            </div>
+              ))
+            ) : (
+              <h2>Sorry, we couldn't find any restaurant for "{restaurantName}"</h2>
+            )}
+          </div>
         </div>
-    );
-};
-
-export default Body;
+      );
+    };
+    
+    export default Body;
